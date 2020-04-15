936fc20b8fa0427de49f8367442192ce2e5d2c6e
Smileyx96 <jasonleezhiyong@gmail.com>
2020-04-16 00:07:25 +0800

    Updates

---
+++ precache-manifest.04e7b66efb935b3d508162f64e7da6c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edd35a3c8eaeeb0ca6341069e413b25",
+    "url": "/Skillset-analysis/index.html"
+  },
+  {
+    "revision": "5e41f4bf7e3d29b2a5dd",
+    "url": "/Skillset-analysis/static/css/main.a5406dfb.chunk.css"
+  },
+  {
+    "revision": "5c60aa85f2296a43c88d",
+    "url": "/Skillset-analysis/static/js/2.4e21514a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Skillset-analysis/static/js/2.4e21514a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e41f4bf7e3d29b2a5dd",
+    "url": "/Skillset-analysis/static/js/main.1ca5e5f1.chunk.js"
+  },
+  {
+    "revision": "4503d39c45d7fee3c6a7",
+    "url": "/Skillset-analysis/static/js/runtime-main.bf684ce4.js"
+  },
+  {
+    "revision": "6dba1c0a21e0d7ac1a38a9a844f6f735",
+    "url": "/Skillset-analysis/static/media/about-layer.6dba1c0a.png"
+  },
+  {
+    "revision": "0ba57ab54d8855a7c0d4194ec48bd4dc",
+    "url": "/Skillset-analysis/static/media/backgroundfyp.0ba57ab5.jpg"
+  },
+  {
+    "revision": "7bc09400fbdc4d4d03f513299f20f719",
+    "url": "/Skillset-analysis/static/media/layer1.7bc09400.png"
+  },
+  {
+    "revision": "13499336ffa89d51e5e9a5fdd9b3de95",
+    "url": "/Skillset-analysis/static/media/layer2.13499336.png"
+  },
+  {
+    "revision": "1f1544b4f6215f75bc732e84c5d77a15",
+    "url": "/Skillset-analysis/static/media/layer3.1f1544b4.png"
+  }
+]);
